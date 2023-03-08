c232423693037fa6b4be18f306cfc34c20794b2d
Mirdad1224 <ali.mirdad@yahoo.com>
2023-03-08 18:14:01 +0330

    add login utility function

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -1,5 +1,6 @@
 import User from "../models/User";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { NextFunction, Request, Response } from "express";
 import checkCredential from "../validators/register";
 import errorGenerate from "../utils/errorGenerate";
@@ -36,6 +37,66 @@ export const register = async (
       errorGenerate("Users data was not saved.Try again...");
     }
     res.status(201).json({ message: `New user created!`, user: { email } });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { email, pwd } = req.body;
+
+  if (!email || !pwd) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  try {
+    const checkData = await checkCredential({ email, pwd });
+    if (checkData !== true) {
+      errorGenerate("Invalid Inputs", 400, checkData);
+    }
+    const foundUser = await User.findOne({ email }).exec();
+    if (!foundUser) {
+      errorGenerate("Invalid email or password", 401);
+    }
+
+    const match = await bcrypt.compare(pwd, foundUser!.password as string);
+
+    if (!match) {
+      errorGenerate("Invalid email or password", 401);
+    }
+
+    const accessToken = jwt.sign(
+      { email: foundUser!.email },
+      process.env.ACCESS_TOKEN_SECRET?.toString()!,
+      { expiresIn: "15m" }
+    );
+
+    const refreshToken = jwt.sign(
+      { email: foundUser!.email },
+      process.env.REFRESH_TOKEN_SECRET?.toString()!,
+      { expiresIn: "7d" }
+    );
+
+    // Saving refreshToken with current user
+    foundUser!.refreshToken = refreshToken;
+    const result = await foundUser!.save();
+    if (!result) {
+      errorGenerate("Users credentials was not saved.Try again...");
+    }
+
+    // Create secure cookie with refresh token
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true, //accessible only by web server
+      //secure: true, //https
+      sameSite: "none", //cross-site cookie
+      maxAge: 7 * 24 * 60 * 60 * 1000, //cookie expiry: set to match rT
+    });
+
+    // Send accessToken
+    res.json({ accessToken });
   } catch (err) {
     next(err);
   }
