a510bd4703ea02b70dca503e40bdc5dcb63aa6fe
Mirdad1224 <ali.mirdad@yahoo.com>
2023-03-10 13:06:54 +0330

    add logout controller for clearing cookie

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -138,3 +138,34 @@ export const refresh = async (
     next(err);
   }
 };
+
+export const logout = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const cookies = req.cookies;
+  if (!cookies?.jwt) return res.sendStatus(204); //No content
+  const refreshToken = cookies.jwt;
+
+  // Is refreshToken in db?
+  try {
+    const foundUser = await User.findOne({ refreshToken }).exec();
+    if (!foundUser) {
+      res.clearCookie("jwt", {
+        httpOnly: true,
+        sameSite: "none",
+        secure: true,
+      });
+      return res.sendStatus(204);
+    }
+
+    // Delete refreshToken in db
+    foundUser.refreshToken = "";
+    const result = await foundUser.save();
+    res.clearCookie("jwt", { httpOnly: true, sameSite: "none", secure: true }); //*
+    res.json({ message: "Cookie cleared" });
+  } catch (err) {
+    next(err);
+  }
+};
