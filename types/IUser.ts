6ba927a38956e4a5afc4aaed015a2cc084bb327b
Mirdad1224 <ali.mirdad@yahoo.com>
2023-06-29 21:07:17 +0330

    refine user interface

--- types/IUser.ts
+++ types/IUser.ts
@@ -1,11 +1,25 @@
-import { Types } from "mongoose";
-
 export default interface IUser {
+  firstName: string;
+  lastName: string;
+  avatar: string;
   email: string;
   password?: string;
-  name?: string;
-  image?: string;
-  phone?: string;
-  conversations?: [Types.ObjectId];
-  refreshToken?: string;
+  passwordChangedAt: DateConstructor | number;
+  passwordResetToken: string | undefined;
+  passwordResetExpires: DateConstructor | undefined;
+  createdAt: DateConstructor | number;
+  updatedAt: DateConstructor;
+  verified: boolean;
+  otp: string | undefined;
+  otp_expiry_time: DateConstructor;
+  friends: any[];
+  socket_id: string;
+  status: "Online" | "Offline";
+}
+
+export interface IUserMethods {
+  correctPassword(candidateOTP: string, userOTP: string): Promise<boolean>;
+  correctOTP(candidateOTP: string, userOTP: string): Promise<boolean>;
+  changedPasswordAfter(JWTTimeStamp: number): boolean;
+  createPasswordResetToken(): string;
 }
